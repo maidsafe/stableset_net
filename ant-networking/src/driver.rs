90b382c01eb206dfe83cd37984a0b4c60e992ec0
Benno Zeeman <bzeeman@live.nl>
2025-01-15 16:06:05 +0100

    refactor: remove `first` again; always add addr
    in local mode.

--- ant-networking/src/driver.rs
+++ ant-networking/src/driver.rs
@@ -269,7 +269,6 @@ pub struct NetworkBuilder {
     keypair: Keypair,
     listen_addr: Option<SocketAddr>,
     local: bool,
-    first: bool,
     #[cfg(feature = "open-metrics")]
     metrics_registries: Option<MetricsRegistries>,
     #[cfg(feature = "open-metrics")]
@@ -280,7 +279,7 @@ pub struct NetworkBuilder {
 }
 
 impl NetworkBuilder {
-    pub fn new(keypair: Keypair, local: bool, first: bool) -> Self {
+    pub fn new(keypair: Keypair, local: bool) -> Self {
         Self {
             bootstrap_cache: None,
             concurrency_limit: None,
@@ -288,7 +287,6 @@ impl NetworkBuilder {
             keypair,
             listen_addr: None,
             local,
-            first,
             #[cfg(feature = "open-metrics")]
             metrics_registries: None,
             #[cfg(feature = "open-metrics")]
@@ -702,7 +700,6 @@ impl NetworkBuilder {
             swarm,
             self_peer_id: peer_id,
             local: self.local,
-            first: self.first,
             is_client,
             is_behind_home_network: self.is_behind_home_network,
             #[cfg(feature = "open-metrics")]
@@ -799,8 +796,6 @@ pub struct SwarmDriver {
     pub(crate) self_peer_id: PeerId,
     /// When true, we don't filter our local addresses
     pub(crate) local: bool,
-    /// When true, we will put our listening addresses in the bootstrap cache.
-    pub(crate) first: bool,
     pub(crate) is_client: bool,
     pub(crate) is_behind_home_network: bool,
     #[cfg(feature = "open-metrics")]

8deebc9629fd03987af9be1541ebe36249ede1db
Roland Sherwin <RolandSherwin@protonmail.com>
2025-01-17 16:51:52 +0530

    feat(metrics): expose health metrics and don't disconnect on low success rate

--- ant-networking/src/metrics/mod.rs
+++ ant-networking/src/metrics/mod.rs
@@ -48,6 +48,7 @@ pub(crate) struct NetworkMetricsRecorder {
     pub(crate) open_connections: Gauge,
     pub(crate) peers_in_routing_table: Gauge,
     pub(crate) records_stored: Gauge,
+    pub(crate) relay_reservation_health: Gauge<f64, AtomicU64>,
 
     // quoting metrics
     relevant_records: Gauge,
@@ -87,6 +88,12 @@ impl NetworkMetricsRecorder {
             "records_stored",
             "The number of records stored locally",
             records_stored.clone(),
+        );
+        let relay_reservation_health = Gauge::<f64, AtomicU64>::default();
+        sub_registry.register(
+            "relay_reservation_health",
+            "The average health of all the relay reservation connections. Value is between 0-1",
+            relay_reservation_health.clone(),
         );
 
         let connected_peers = Gauge::default();
@@ -226,6 +233,7 @@ impl NetworkMetricsRecorder {
             estimated_network_size,
             connected_peers,
             open_connections,
+            relay_reservation_health,
             peers_in_routing_table,
             relevant_records,
             max_records,

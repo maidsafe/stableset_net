maidsafe/autonomi

feat(client): implement ClientOperationCfg to control any client operation

ant-cli/src/commands/file.rs
@@ -10,13 +10,15 @@ use crate::network::NetworkPeers;
 use crate::utils::collect_upload_summary;
 use crate::wallet::load_wallet;
 use autonomi::client::address::addr_to_str;
+use autonomi::client::config::ClientOperationConfig;
+use autonomi::ResponseQuorum;
 use color_eyre::eyre::Context;
 use color_eyre::eyre::Result;
 use color_eyre::Section;
 use std::path::PathBuf;
 
 pub async fn cost(file: &str, peers: NetworkPeers) -> Result<()> {
-    let client = crate::actions::connect_to_network(peers).await?;
+    let client = crate::actions::connect_to_network(peers, Default::default()).await?;
 
     println!("Getting upload cost...");
     info!("Calculating cost for file: {file}");
@@ -31,9 +33,19 @@ pub async fn cost(file: &str, peers: NetworkPeers) -> Result<()> {
     Ok(())
 }
 
-pub async fn upload(file: &str, public: bool, peers: NetworkPeers) -> Result<()> {
-    let mut client = crate::actions::connect_to_network(peers).await?;
-    let wallet = load_wallet(&client.evm_network)?;
+pub async fn upload(
+    file: &str,
+    public: bool,
+    peers: NetworkPeers,
+    verification_quorum: Option<ResponseQuorum>,
+) -> Result<()> {
+    let mut client_operation_config = ClientOperationConfig::default();
+    if let Some(verification_quorum) = verification_quorum {
+        client_operation_config.chunk_verification_quorum(verification_quorum);
+    }
+    let mut client = crate::actions::connect_to_network(peers, client_operation_config).await?;
+
+    let wallet = load_wallet(client.get_evm_network())?;
     let event_receiver = client.enable_client_events();
     let (upload_summary_thread, upload_completed_tx) = collect_upload_summary(event_receiver);
 
@@ -105,8 +117,18 @@ pub async fn upload(file: &str, public: bool, peers: NetworkPeers) -> Result<()>
     Ok(())
 }
 
-pub async fn download(addr: &str, dest_path: &str, peers: NetworkPeers) -> Result<()> {
-    let mut client = crate::actions::connect_to_network(peers).await?;
+pub async fn download(
+    addr: &str,
+    dest_path: &str,
+    peers: NetworkPeers,
+    read_quorum: Option<ResponseQuorum>,
+) -> Result<()> {
+    let mut client_operation_config = ClientOperationConfig::default();
+    if let Some(read_quorum) = read_quorum {
+        client_operation_config.chunk_read_quorum(read_quorum);
+    }
+    let mut client = crate::actions::connect_to_network(peers, client_operation_config).await?;
+
     crate::actions::download(addr, dest_path, &mut client).await
 }
 

autonomi/src/client/mod.rs
@@ -15,6 +15,8 @@
 /// - Pointer
 /// - Scratchpad
 pub mod data_types;
+use config::ClientConfig;
+use config::ClientOperationConfig;
 pub use data_types::chunk;
 pub use data_types::graph;
 pub use data_types::pointer;
@@ -28,6 +30,7 @@ pub use high_level::files;
 pub use high_level::vault;
 
 pub mod address;
+pub mod config;
 pub mod payment;
 pub mod quote;
 
@@ -79,34 +82,13 @@ pub struct Client {
     pub(crate) network: Network,
     pub(crate) client_event_sender: Arc<Option<mpsc::Sender<ClientEvent>>>,
     /// The EVM network to use for the client.
-    pub evm_network: EvmNetwork,
+    evm_network: Arc<EvmNetwork>,
+    /// The configuration for operations on the client.
+    ///
+    /// This will be shared across all clones of the client.
+    operation_config: Arc<ClientOperationConfig>,
     // Shutdown signal for the `SwarmDriver` task
     _shutdown_tx: watch::Sender<bool>,
-}
-
-/// Configuration for [`Client::init_with_config`].
-#[derive(Debug, Clone, Default)]
-pub struct ClientConfig {
-    /// Whether we're expected to connect to a local network.
-    pub local: bool,
-
-    /// List of peers to connect to.
-    ///
-    /// If not provided, the client will use the default bootstrap peers.
-    pub peers: Option<Vec<Multiaddr>>,
-
-    /// EVM network to use for quotations and payments.
-    pub evm_network: EvmNetwork,
-}
-
-impl ClientConfig {
-    fn local(peers: Option<Vec<Multiaddr>>) -> Self {
-        Self {
-            local: true,
-            peers,
-            evm_network: EvmNetwork::new(true).unwrap_or_default(),
-        }
-    }
 }
 
 /// Error returned by [`Client::init`].
@@ -199,6 +181,7 @@ impl Client {
             local,
             peers: Some(peers),
             evm_network: EvmNetwork::new(local).unwrap_or_default(),
+            operation_config: Default::default(),
         })
         .await
     }
@@ -251,7 +234,8 @@ impl Client {
         Ok(Self {
             network,
             client_event_sender: Arc::new(None),
-            evm_network: config.evm_network,
+            evm_network: Arc::new(config.evm_network),
+            operation_config: Arc::new(config.operation_config),
             _shutdown_tx: shutdown_tx,
         })
     }
@@ -264,6 +248,10 @@ impl Client {
         debug!("All events to the clients are enabled");
 
         client_event_receiver
+    }
+
+    pub fn get_evm_network(&self) -> &EvmNetwork {
+        &self.evm_network
     }
 }
 

maidsafe/stableset_net

ant-cli/src/commands/file.rs
@@ -10,13 +10,15 @@ use crate::network::NetworkPeers;
 use crate::utils::collect_upload_summary;
 use crate::wallet::load_wallet;
 use autonomi::client::address::addr_to_str;
+use autonomi::client::config::ClientOperationConfig;
+use autonomi::ResponseQuorum;
 use color_eyre::eyre::Context;
 use color_eyre::eyre::Result;
 use color_eyre::Section;
 use std::path::PathBuf;
 
 pub async fn cost(file: &str, peers: NetworkPeers) -> Result<()> {
-    let client = crate::actions::connect_to_network(peers).await?;
+    let client = crate::actions::connect_to_network(peers, Default::default()).await?;
 
     println!("Getting upload cost...");
     info!("Calculating cost for file: {file}");
@@ -31,9 +33,19 @@ pub async fn cost(file: &str, peers: NetworkPeers) -> Result<()> {
     Ok(())
 }
 
-pub async fn upload(file: &str, public: bool, peers: NetworkPeers) -> Result<()> {
-    let mut client = crate::actions::connect_to_network(peers).await?;
-    let wallet = load_wallet(&client.evm_network)?;
+pub async fn upload(
+    file: &str,
+    public: bool,
+    peers: NetworkPeers,
+    verification_quorum: Option<ResponseQuorum>,
+) -> Result<()> {
+    let mut client_operation_config = ClientOperationConfig::default();
+    if let Some(verification_quorum) = verification_quorum {
+        client_operation_config.chunk_verification_quorum(verification_quorum);
+    }
+    let mut client = crate::actions::connect_to_network(peers, client_operation_config).await?;
+
+    let wallet = load_wallet(client.evm_network())?;
     let event_receiver = client.enable_client_events();
     let (upload_summary_thread, upload_completed_tx) = collect_upload_summary(event_receiver);
 
@@ -105,8 +117,18 @@ pub async fn upload(file: &str, public: bool, peers: NetworkPeers) -> Result<()>
     Ok(())
 }
 
-pub async fn download(addr: &str, dest_path: &str, peers: NetworkPeers) -> Result<()> {
-    let mut client = crate::actions::connect_to_network(peers).await?;
+pub async fn download(
+    addr: &str,
+    dest_path: &str,
+    peers: NetworkPeers,
+    read_quorum: Option<ResponseQuorum>,
+) -> Result<()> {
+    let mut client_operation_config = ClientOperationConfig::default();
+    if let Some(read_quorum) = read_quorum {
+        client_operation_config.chunk_read_quorum(read_quorum);
+    }
+    let mut client = crate::actions::connect_to_network(peers, client_operation_config).await?;
+
     crate::actions::download(addr, dest_path, &mut client).await
 }
 

autonomi/src/client/mod.rs
@@ -15,6 +15,8 @@
 /// - Pointer
 /// - Scratchpad
 pub mod data_types;
+use config::ClientConfig;
+use config::ClientOperationConfig;
 pub use data_types::chunk;
 pub use data_types::graph;
 pub use data_types::pointer;
@@ -29,6 +31,7 @@ pub use high_level::register;
 pub use high_level::vault;
 
 pub mod address;
+pub mod config;
 pub mod key_derivation;
 pub mod payment;
 pub mod quote;
@@ -80,34 +83,13 @@ pub struct Client {
     pub(crate) network: Network,
     pub(crate) client_event_sender: Arc<Option<mpsc::Sender<ClientEvent>>>,
     /// The EVM network to use for the client.
-    pub evm_network: EvmNetwork,
+    evm_network: Arc<EvmNetwork>,
+    /// The configuration for operations on the client.
+    ///
+    /// This will be shared across all clones of the client.
+    operation_config: Arc<ClientOperationConfig>,
     // Shutdown signal for child tasks. Sends signal when dropped.
     _shutdown_tx: watch::Sender<bool>,
-}
-
-/// Configuration for [`Client::init_with_config`].
-#[derive(Debug, Clone, Default)]
-pub struct ClientConfig {
-    /// Whether we're expected to connect to a local network.
-    pub local: bool,
-
-    /// List of peers to connect to.
-    ///
-    /// If not provided, the client will use the default bootstrap peers.
-    pub peers: Option<Vec<Multiaddr>>,
-
-    /// EVM network to use for quotations and payments.
-    pub evm_network: EvmNetwork,
-}
-
-impl ClientConfig {
-    fn local(peers: Option<Vec<Multiaddr>>) -> Self {
-        Self {
-            local: true,
-            peers,
-            evm_network: EvmNetwork::new(true).unwrap_or_default(),
-        }
-    }
 }
 
 /// Error returned by [`Client::init`].
@@ -200,6 +182,7 @@ impl Client {
             local,
             peers: Some(peers),
             evm_network: EvmNetwork::new(local).unwrap_or_default(),
+            operation_config: Default::default(),
         })
         .await
     }
@@ -256,7 +239,8 @@ impl Client {
         Ok(Self {
             network,
             client_event_sender: Arc::new(None),
-            evm_network: config.evm_network,
+            evm_network: Arc::new(config.evm_network),
+            operation_config: Arc::new(config.operation_config),
             _shutdown_tx: shutdown_tx,
         })
     }
@@ -269,6 +253,10 @@ impl Client {
         debug!("All events to the clients are enabled");
 
         client_event_receiver
+    }
+
+    pub fn evm_network(&self) -> &EvmNetwork {
+        &self.evm_network
     }
 }
 